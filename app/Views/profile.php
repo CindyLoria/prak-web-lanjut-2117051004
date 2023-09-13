78b86cffcba54ec5e0eb79ca54b76d2079fead50
cindyloria <92240811+CindyLoria@users.noreply.github.com>
2023-09-13 17:48:35 +0700

    Ganti profile

--- app/Views/profile.php
+++ app/Views/profile.php
@@ -4,25 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Ini Halaman Profile</h1>
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><?= $nama ?></td>
-        </tr>
-        <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td><?= $kelas ?></td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><?= $npm ?></td>
-        </tr>
-    </table>
+    <h1><center>BIODATA</center></h1>
+    <br>
+    <br>
+    <div style="text-align:center;">
+        <img src="<?php echo base_url('cindy.jpg'); ?>" alt="cindy" style="height: 250px; width: 250px; border-radius: 50%; object-fit: cover;"  border="1.5px" ><br>
+        <br>
+        <br>
+        <button type="button" class="btn btn-outline-dark" style="width: 250px;">Nama : <?= $nama ?></button><br><br>
+        <button type="button" class="btn btn-outline-dark" style="width: 250px;">Kelas : <?= $kelas ?></button><br><br>
+        <button type="button" class="btn btn-outline-dark" style="width: 250px;">NPM : <?= $npm ?></button><br><br>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
