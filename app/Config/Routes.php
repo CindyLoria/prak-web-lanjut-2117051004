d72675020f9c5ed47aa291292103eaca4d616884
CindyLoria <cindyloria091@gmail.com>
2023-10-28 15:33:19 +0700

    crud kelas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\UserController; 
+use App\Controllers\KelasController; 
 use App\Controllers\Home;
 use CodeIgniter\Router\RouteCollection;
 
@@ -20,3 +21,11 @@ $routes->put('/user/(:any)', [UserController::class, 'update']);
 $routes->delete('/user/(:any)', [UserController::class, 'destroy']);
 
 $routes->get('user/(:any)', [UserController::class, 'show']);
+
+$routes->get('/kelas/create', [KelasController::class,'create']);
+$routes->post('/kelas/store', [KelasController::class,'store']);
+$routes->match(['get', 'post'],'/kelas/store', [KelasController::class, 'store']);
+$routes->get('/kelas', [KelasController::class,'index']);
+$routes->get('/kelas/(:any)/edit', [KelasController::class, 'edit']);
+$routes->put('/kelas/(:any)', [KelasController::class, 'update']);
+$routes->delete('/kelas/(:any)', [KelasController::class, 'destroy']);
