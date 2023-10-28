5ff5ac805245bb99bbf9fd9b49cbb6f5fa0ca02c
CindyLoria <cindyloria091@gmail.com>
2023-10-28 21:55:22 +0700

    navigation panel

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="<?= base_url("assets/css/style.css")?>">
 </head>
 <body>
+    
     <?= $this->renderSection('content') ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
